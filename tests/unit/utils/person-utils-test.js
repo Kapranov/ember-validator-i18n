73a1268ee6e6f7b851303b65a0b3c2d82aff057f
Oleg G.Kapranov <oleg@vertiopia.com>
2016-11-29 18:41:11 +0200

    Generated a new route, controller, model, util

---
+++ tests/unit/utils/person-utils-test.js
@@ -0,0 +1,10 @@
+import personUtils from 'ember-validator-i18n/utils/person-utils';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | person utils');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = personUtils();
+  assert.ok(result);
+});
